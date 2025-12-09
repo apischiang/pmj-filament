1019e830ccf10d112d71435ce96c1afe358417cf
haafidz <haafidz123@gmail.com>
2025-12-09 23:04:39 +0700

    tambah migrasi customer

---
+++ database/migrations/2025_12_09_155124_create_customers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->string('uid', 20)->primary();
+            $table->string('name', 20);
+            $table->string('company_name', 50)->nullable();
+            $table->text('address');
+            $table->string('npwp', 16)->unique()->index();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
